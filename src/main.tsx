e757fd9398a2dcc51bf1cfe1b29eec4d0801144c
MATHIEU FOUCAULT <foucault.mathieu.pro@gmail.com>
2024-10-30 11:06:35 +0100

    feat : use effect vs loaders

--- src/main.tsx
+++ src/main.tsx
@@ -9,11 +9,63 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Article from "./pages/Article";
 
+// CETTE PARTIE N'EST A UTILISER QUE SI ON SE SERT DES LOADERS DE REACT ROUTER :
+//ICI POUR LA METEO DE LA PAGE HOME:
+const getWeatherOfTheDay = () => {
+	return "sunny";
+};
+//////////////////////////////////
+
+//ICI POUR L'AFFICHAGE DES DONNEES DES ARTICLES :
+
+const allData = [
+	{
+		id: 1,
+		title: "Lorem Ipsum",
+		content: "Lorem ipsum dolor sit amet",
+	},
+	{
+		id: 2,
+		title: "Schnapsum",
+		content: "Lorem Elsass ipsum Salut bisamme",
+	},
+	{
+		id: 3,
+		title: "Cupcake Ipsum",
+		content: "Tiramisu pastry wafer brownie soufflé",
+	},
+];
+
+type Data = (typeof allData)[0];
+
+const getSomeData = (id: number) => {
+	return allData.find((article) => article.id === id) as Data | null;
+};
+
+//////////////////////////////////////////////////////////////
+
 // router creation
 
 const router = createBrowserRouter([
+	// AVEC UN LOADER LOCAL SUR LE CHEMIN / :
+	// {
+	// 	element: <App />,
+	// 	children: [
+	// 		{
+	// 			path: "/",
+	// 			element: <Home />,
+	// 			loader: () => {
+	// 				return getWeatherOfTheDay();
+	// 			},
+	// 		},
+
+	//AVEC UN LOADER GLOBAL :
 	{
 		element: <App />,
+		loader: () => {
+			return getWeatherOfTheDay();
+		},
+		id: "app",
 		children: [
 			{
 				path: "/",
@@ -26,6 +78,11 @@ const router = createBrowserRouter([
 			{
 				path: "/articles/:id",
 				element: <Article />,
+				loader: ({ params }) => {
+					const idAsInt = Number.parseInt(params.id ?? "0");
+
+					return getSomeData(idAsInt);
+				},
 			},
 		],
 	},
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,31 @@
+// UTILISATION DU HOOK USERLOADERDATA DANS LE CADRE D'UN LOADER LOCAL :
+
+// import { useLoaderData } from "react-router-dom";
+
+// function Home() {
+// 	const weather = useLoaderData() as string;
+
+// 	return (
+// 		<>
+// 			<h1>Hello from Home</h1>
+// 			<p>Today is a {weather} day</p>
+// 		</>
+// 	);
+// }
+
+// export default Home;
+
+import { useRouteLoaderData } from "react-router-dom";
+
 function Home() {
-	return <h1>Hello from Home</h1>;
+	const weather = useRouteLoaderData("app") as string;
+
+	return (
+		<>
+			<h1>Hello from Home</h1>
+			<p>Today is a {weather} day</p>
+		</>
+	);
 }
 
 export default Home;
